3b9f7c07e39e9314dd12d2f7cb1c3eb19e6bd9f2
Anas Bader <anas.bader@insat.ucar.tn>
2024-05-07 00:46:21 +0100

    add css to login and register pages

--- FRONTEND/src/components/Login.js
+++ FRONTEND/src/components/Login.js
@@ -1,67 +1,94 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { Form, Input, Button } from "antd";
+import { Link } from "react-router-dom";
+import "./css/Login.css";
 
 const Login = () => {
+  const [errorMessage, setErrorMessage] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
 
   const removeHtmlEntities = (input) => {
     const pattern = /&[^\s]*?;/g;
     return input.replace(pattern, "");
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    if (!email || !password) {
-      setErrorMessage("Please provide both email and password.");
-      return;
-    }
-
+  const handleSubmit = async (values) => {
     try {
       const response = await axios.post(
         "http://localhost:8080/api/v1/auth/authenticate",
         {
-          email: (email.trim()),
-          password: removeHtmlEntities(password.trim()),
+          email: removeHtmlEntities(email.trim()),
+          password: password,
         }
       );
 
       const token = response.data.token;
-      // Store the token in session storage
       sessionStorage.setItem("token", token);
-      // Redirect to home page or perform any other action after successful login
       console.log("Login successful");
       window.location.href = "/";
     } catch (error) {
       console.error("Login failed:", error);
-      // Handle login failure
       setErrorMessage("Login failed. Please check your credentials.");
     }
   };
 
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit" onClick={(e) => handleSubmit(e)}>
-          Login
-        </button>
-      </form>
-      {errorMessage && <p>{errorMessage}</p>}
+    <div className="login-page">
+      <div className="login-box">
+        <div className="illustration-wrapper">
+          <img
+            src="https://mixkit.imgix.net/art/preview/mixkit-left-handed-man-sitting-at-a-table-writing-in-a-notebook-27-original-large.png?q=80&auto=format%2Ccompress&h=700"
+            alt="Login"
+          />
+        </div>
+        <Form
+          name="login-form"
+          initialValues={{ remember: true }}
+          handleSubmit={handleSubmit}
+          onFinishFailed={onFinishFailed}
+        >
+          <p className="form-title">Welcome back</p>
+          <Form.Item
+            name="email"
+            value="email"
+            onChange={(e) => setEmail(e.target.value)}
+            rules={[{ required: true, message: "Please input your email!" }]}
+          >
+            <Input placeholder="Email" />
+          </Form.Item>
+
+          <Form.Item
+            name="password"
+            value="password"
+            onChange={(e) => setPassword(e.target.value)}
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password placeholder="Password" />
+          </Form.Item>
+
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+              onClick={(e) => handleSubmit(e)}
+            >
+              LOGIN
+            </Button>
+          </Form.Item>
+          <p>
+            Don't have an account? Click here to{" "}
+            <Link to="/register">Register</Link>
+          </p>
+        </Form>
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
+      </div>
     </div>
   );
 };
--- FRONTEND/src/pages/RegisterPage.js
+++ FRONTEND/src/pages/RegisterPage.js
@@ -4,7 +4,6 @@ import Register from "../components/Register.js";
 function RegisterPage() {
   return (
     <div>
-      <h1>Welcome to Our Website</h1>
       <Register />
     </div>
   );
